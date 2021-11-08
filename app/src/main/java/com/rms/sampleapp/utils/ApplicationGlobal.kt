2a5c5dccff027718a22f77d006326e8b420202bd
Mukesh Rana <mukesh@kachyng.com>
2021-11-08 17:26:25 +0530

    removed test credentials

--- app/src/main/java/com/rms/sampleapp/utils/ApplicationGlobal.kt
+++ app/src/main/java/com/rms/sampleapp/utils/ApplicationGlobal.kt
@@ -16,12 +16,12 @@ class ApplicationGlobal : Application() {
 
         RmsClient.configure(
             this, RmsConfiguration(
-                clientID = "3c65r36dinkd5n8fo4ud4829cn",
-                clientSecret = "13dpgtvgfnqsn43drq20tdlvttps5m44luha7hauj6d6djua8j7f",
-                callbackUrl = "rmssdk://www.rmssdk.com/callback",
-                signOutUrl = "rmssdk://www.rmssdk.com/callback_signout",
-                baseUrl = "https://api-terminal-sandbox.retailmerchantservices.net/",
-                oAuthUrl = "api-auth-dev.retailmerchantservices.net"
+                clientID = "yourClientId",
+                clientSecret = "yourClientSecret",
+                callbackUrl = "yourCallbackUrl",
+                signOutUrl = "yourSignOutUrl",
+                baseUrl = "yourBaseUrl",
+                oAuthUrl = "yourOauthUrl"
             )
         )
 
